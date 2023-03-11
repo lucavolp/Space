fd8dde48271b230d2251e1c483df7f47043f9818
Mr 4B <88139331+Mr4B@users.noreply.github.com>
2023-03-11 15:24:07 +0100

    Se non ti piacciono cancellali

---
+++ MyPanel.java
@@ -0,0 +1,33 @@
+
+/**
+ * Aggiungi qui una descrizione della classe MyPanel
+ * 
+ * @author (il tuo nome) 
+ * @version (un numero di versione o una data)
+ */
+public class MyPanel
+{
+    // variabili d'istanza - sostituisci l'esempio che segue con il tuo
+    private int x;
+
+    /**
+     * Costruttore degli oggetti di classe  MyPanel
+     */
+    public MyPanel()
+    {
+        // inizializza le variabili d'istanza
+        x = 0;
+    }
+
+    /**
+     * Un esempio di metodo - aggiungi i tuoi commenti
+     * 
+     * @param  y   un parametro d'esempio per un metodo
+     * @return     la somma di x e y
+     */
+    public int sampleMethod(int y)
+    {
+        // metti qui il tuo codice
+        return x + y;
+    }
+}
---
+++ Test.java
@@ -0,0 +1,33 @@
+
+/**
+ * Aggiungi qui una descrizione della classe Test
+ * 
+ * @author (il tuo nome) 
+ * @version (un numero di versione o una data)
+ */
+public class Test
+{
+    // variabili d'istanza - sostituisci l'esempio che segue con il tuo
+    private int x;
+
+    /**
+     * Costruttore degli oggetti di classe  Test
+     */
+    public Test()
+    {
+        // inizializza le variabili d'istanza
+        x = 0;
+    }
+
+    /**
+     * Un esempio di metodo - aggiungi i tuoi commenti
+     * 
+     * @param  y   un parametro d'esempio per un metodo
+     * @return     la somma di x e y
+     */
+    public int sampleMethod(int y)
+    {
+        // metti qui il tuo codice
+        return x + y;
+    }
+}
