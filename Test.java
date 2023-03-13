038607f16c3d51f5135e72c8670f73255a93b27b
lucavolp <127590122+lucavolp@users.noreply.github.com>
2023-03-13 18:57:53 +0000

    modified:   Test.java 	new file:   lib/junit-platform-console-standalone-1.9.2.jar

--- Test.java
+++ Test.java
@@ -4,11 +4,10 @@
  * @author (Battistelli Kevin - Volpinari Luca)
  * @version (1.0)
  */
-import java.io.*;
 public class Test {
     /*
-    public Test() {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        System.out.println("hello World!");
     }*/
 
     public static void main(String[] args) {
