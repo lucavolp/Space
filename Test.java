f4c6670ea0188014544636c18b4c359f71299f14
Mr 4B <88139331+Mr4B@users.noreply.github.com>
2023-03-13 10:24:27 +0000

    modified:   Test.java

--- Test.java
+++ Test.java
@@ -9,4 +9,8 @@ public class Test {
     public Test() {
         System.out.println("Hello World!");
     }
+
+    public static void main(String[] args) {
+        System.out.println("Ciao");
+    }
 }
