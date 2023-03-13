b065d08bd9622cf3937f6e231fb2f3e0b027b9cb
Mr 4B <88139331+Mr4B@users.noreply.github.com>
2023-03-13 11:10:16 +0000

    modified:   Test.java

--- Test.java
+++ Test.java
@@ -6,9 +6,10 @@
  */
 import java.io.*;
 public class Test {
+    /*
     public Test() {
         System.out.println("Hello World!");
-    }
+    }*/
 
     public static void main(String[] args) {
         System.out.println("Ciao");
