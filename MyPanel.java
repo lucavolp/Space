0942de4e1c90d1fcc2b4e5d18a04a0214ddc319e
lucavolp <s_vlplcu05e21z130c@itisurbino.it>
2023-05-14 20:54:05 +0200

    a

--- MyPanel.java
+++ MyPanel.java
@@ -15,7 +15,10 @@ import javax.imageio.*;
 public class MyPanel extends JPanel //implements ActionListener 
 {
     private MyFrame frame;
-    private JLabel lillo;
+    private JLabel lillo;   //Label vuota per impaginazione
+    private JLabel lillo2;  //Label vuota per impaginazione
+    private JLabel lillo3;  //Label vuota per impaginazione
+    private JLabel lillo4;  //Label vuota per impaginazione
     private JButton change;
     private JButton chiudi;
     public AscoltatoreEsterno as;
@@ -35,27 +38,44 @@ public class MyPanel extends JPanel //implements ActionListener
         
         frame = f;
         as= new AscoltatoreEsterno(this, frame);
-        lillo = new JLabel("Panel 1");
-        add(lillo);
+        lillo = new JLabel("       ");
+        lillo2 = new JLabel("       ");
+        lillo3 = new JLabel("       ");
+        lillo4 = new JLabel("       ");
+        //add(lillo);
         change = new JButton("New Game");
         change.addActionListener(as);
-        add(change); //passa di là
+        //add(change); //passa di là
         chiudi = new JButton("Chiudi");
         chiudi.addActionListener(as);
-        add(chiudi);
+        //add(chiudi);
         
         //Layout home
         setLayout(new GridBagLayout());
         
+        lillo.setPreferredSize(new Dimension(300,50));
+        change.setPreferredSize(new Dimension(300,50));
+        chiudi.setPreferredSize(new Dimension(300,50));
         
         GridBagConstraints c = new GridBagConstraints();
-        c.gridwidth = 3;
         c.anchor = GridBagConstraints.CENTER;
-        add(lillo, c);
-        //c.gridwidth = 1;
+        c.gridx = 0;
+        c.gridy = 0;
+        add(lillo2, c);
         c.gridy = 1;
+        add(lillo3, c);
+        c.gridy = 2;
+        add(lillo4, c);
+        
+        
+        c.gridy = 3;
+        c.gridx = 0;
         add(change, c);
-        c.gridx = 3;
+        c.gridy = 3;
+        c.gridx = 1;
+        add(lillo, c);
+        c.gridy = 3;
+        c.gridx = 2;
         add(chiudi, c);
         
         /*
